fab08d1d5383816c40ee7802aa569e3ff4cd796d
Vincent <vincentb45@gmail.com>
2022-01-06 08:05:56 +0100

    feat: add lua configuration for nvim

---
+++ nvim/lua/settings.lua
@@ -0,0 +1,24 @@
+-- display line number
+vim.opt.number = true
+vim.opt.relativenumber = true
+-- use space as a the leader key
+vim.g.mapleader = ' '
+-- highlight the current line
+vim.opt.cursorline = true
+-- highlight trailing characters
+vim.opt.listchars = {eol = '↲', tab = '▸ ', trail = '·'}
+
+-- bindings
+vim.api.nvim_set_keymap('n', '<space>', '<nop>', { noremap = true })
+
+vim.cmd [[
+  set tabstop=4
+  set shiftwidth=4
+  set softtabstop=4
+  set expandtab
+  set autoindent
+  set smartindent
+  set clipboard=unnamedplus
+  set list listchars=tab:»·,trail:·
+  ]]
+
